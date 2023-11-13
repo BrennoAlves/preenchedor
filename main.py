5c3a548560d23ee96dcad5ebc3b470e29aabb6dd
Brenno Nogueira Alves <41513440+BrennoAlves@users.noreply.github.com>
2023-11-13 15:57:00 -0300

    Ajustes

--- main.py
+++ main.py
@@ -3,73 +3,68 @@ import roletas
 import time
 import random
 
-# Sorteia um tempo aleatório entre 1 e 3 minutos
-
-
 # Verifica se o tempo já passou
 while True:
     # Faz alguma coisa enquanto espera o tempo acabar
-    tempo = random.randint(60, 180)
+    tempo = random.randint(45, 75)
     time.sleep(tempo)
 
-    #Função
-    def run(playwright : Playwright) -> None:
+    # Função
+    def run(playwright: Playwright) -> None:
         browser = playwright.chromium.launch(headless=True)
         context = browser.new_context()
         page = context.new_page()
-        page.goto("https://docs.google.com/forms/d/e/1FAIpQLSeyAtCtdop1EfcStNz6Lee6LzqdRNI1HHNpDwHkseHXVGuOeQ/viewform")
+        page.goto(
+            "https://docs.google.com/forms/d/e/1FAIpQLSeyAtCtdop1EfcStNz6Lee6LzqdRNI1HHNpDwHkseHXVGuOeQ/viewform")
 
-
-        #sorteando idade
+        # sorteando idade
         page.get_by_text(roletas.roletaIdade()).click()
 
-        #sorteando renda
+        # sorteando renda
         page.get_by_label(roletas.roletaRenda()).click()
 
-        #sorteando se treina ou não
+        # sorteando se treina ou não
         b = roletas.roletaTreinamento()
         page.get_by_label(b).click()
 
-        #passando de pagina
+        # passando de pagina
         page.get_by_role("button", name="Próxima").click()
 
-        #esperando carregar
+        # esperando carregar
         page.wait_for_load_state("load")
 
-        #caminho para a pessoa que já treina
+        # caminho para a pessoa que já treina
         if b == "Sim":
 
-            #sorteando o valor que a pessoa paga
+            # sorteando o valor que a pessoa paga
             page.get_by_text(roletas.roletaPagamento()).click()
 
-            #sorteando o horario que a pessoa frequenta
+            # sorteando o horario que a pessoa frequenta
             page.get_by_text(roletas.roletaHorario()).click()
 
-            #sorteando a recorrencia que pessoa frequenta
+            # sorteando a recorrencia que pessoa frequenta
             page.get_by_text(roletas.roletaRecorrencia()).click()
 
-            #sorteando o que faria a pessoa aumentar a frequencia
+            # sorteando o que faria a pessoa aumentar a frequencia
             page.get_by_text(roletas.aumentarRecorrencia()).click()
 
-
-        #caminho para a pessoa que não treina
+        # caminho para a pessoa que não treina
         else:
 
-            #sorteando o motivo de não treinar
+            # sorteando o motivo de não treinar
             page.get_by_text(roletas.pqnt()).click()
 
-            #sorteando o quanto pagaria
+            # sorteando o quanto pagaria
             page.get_by_text(roletas.disposicaoPagamento()).click()
 
-        #enviar o forms
+        # enviar o forms
         page.get_by_role("button", name="Enviar").click()
 
-        #fechando o navegador
+        # fechando o navegador
         context.close()
         browser.close()
 
     with sync_playwright() as playwright:
         run(playwright)
-
 
     print('Concluido')
--- roletas.py
+++ roletas.py
@@ -2,19 +2,19 @@ import random
 
 def roletaIdade():
     idades = ["Abaixo dos 19 anos", "Entre 18 e 20 anos", "Entre 21 e 25 anos", "Entre 26 e 30 anos", "Entre 30 e 35 anos", "Entre 40 e 45 anos", "Entre 45 e 50 anos", "Entre 55 e 60 anos", "Mais do que 60 anos"]
-    pesos = [1, 1, 1, 1, 1, 1, 1, 1, 1]
+    pesos = [1, 1.3, 1.5, 1.4, 1, 1, 1, 1, 1]
     idadeRoletada = random.choices(idades, weights=pesos, k=1)
     return idadeRoletada[0]
 
 def roletaRenda():
     rendas = ["Abaixo de 1 salário mínimo", "Entre 1 e 2 salários mínimos", "Entre 2 e 3 salários mínimos", "Entre 3 e 4 salários minímos", "Entre 4 e 5 salários minímos", "Mais que 5 salários minímos"]
-    pesos = [1, 1, 1, 1, 1, 1] 
+    pesos = [1, 1, 1, 1, 0.8, 0.5] 
     rendaRoletada = random.choices(rendas, weights=pesos, k=1)
     return rendaRoletada[0]
 
 def roletaTreinamento():
     treinamento = ["Sim","Não"]
-    pesos = [1,1]
+    pesos = [0.6,1]
     treinamentoRoletado = random.choices(treinamento, weights=pesos, k=1)
     return treinamentoRoletado[0]
 
@@ -22,13 +22,13 @@ def roletaTreinamento():
 #Caminho caso tenha marcado Sim no "Você treina atualmente?"
 def roletaPagamento():
     pagamentos = ["Menos que 70 reais", "Entre 70 e 80 reais", "Entre 81 e 90 reais", "Entre 91 e 100 reais", "Entre 101 e 110 reais", "Acima de 111 reais"]
-    pesos = [1, 1, 1, 1, 1, 1]
+    pesos = [1, 1.5, 1.5, 1, 0.8, 0.8]
     pagamentoRoletado = random.choices(pagamentos, weights=pesos, k=1)
     return pagamentoRoletado[0]
 
 def roletaHorario():
     horarios = ["Antes das 6:00 horas", "Entre 6:00 e 8:00", "Entre 8:00 e 12:00", "Entre 12:00 e 18:00", "Entre 18:00 e 22:00", "Entre 22:00 e 00:00"]
-    pesos = [1, 1, 1, 1, 1, 1]
+    pesos = [1, 1, 1, 1.2, 1.2, 1]
     horarioRoletado = random.choices(horarios, weights=pesos, k=1)
     return horarioRoletado[0]
 
@@ -40,7 +40,7 @@ def roletaRecorrencia():
 
 def aumentarRecorrencia():
     aumentarRec = ["Maior disponibilidade de horário", "Oferecer descontos para usuários frequentes", "Ampliar a variedade de aulas e atividades", "Investir na modernização e ampliação das instalações", "Realizar desafios de superação com prêmios atrativos", "Estou satisfeito com a minha frequência"]
-    pesos = [1, 1, 1, 1, 1, 1]
+    pesos = [2, 1, 1, 1, 1, 0.5]
     aumentarRecRoleta = random.choices(aumentarRec, weights=pesos, k=1)
     return aumentarRecRoleta[0]
 
@@ -49,13 +49,13 @@ def aumentarRecorrencia():
 #Por que não treina?
 def pqnt():
     porques = ["Falta de interesse", "Horários não atende minha necessidade", "Custo Elevado", "Falta de Conveniência na Localização"]
-    pesos = [1,1,1,1]
+    pesos = [1,2,1,1]
     pqntRoletado = random.choices(porques, weights=pesos, k=1)
     return pqntRoletado[0]
 
 def disposicaoPagamento():
     disposicao = ["Menos que 70 reais", "Entre 70 e 80 reais", "Entre 81 e 90 reais", "Entre 91 e 100 reais", "Entre 101 e 110 reais", "Acima de 111 reais"]
-    pesos = [1, 1, 1, 1, 1, 1]
+    pesos = [1, 1, 3, 1, 1, 1]
     disposicaoRoletada = random.choices(disposicao, weights=pesos, k=1)
     return disposicaoRoletada[0]
 
